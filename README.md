81d998b7a87dc59533b0d7ba052631dd7a77a822
Chris Ridenour <chrisridenour@gmail.com>
2012-08-28 01:33:08 -0400

    Working prototype. No centering or ideal sizes yet

---
+++ README.md
@@ -0,0 +1,10 @@
+Place Chris
+============
+
+Sometimes it's hard to let go. That's why I've created this helpful tool to let you place images into your development sites with my smiling face.
+
+Coming soon
+------------
+* PlaceChris Ad Replacer
+* Chrome & Firefox Themes
+* PlaceJohnDavidBack
--- generator/index.js
+++ generator/index.js
@@ -19,7 +19,6 @@ exports.list = function(req, res) {
 				images: images
 			});
 
-			console.log(images)
 		}
 	],
 	function(err, result) {
@@ -29,15 +28,62 @@ exports.list = function(req, res) {
 
 exports.generate = function(req, res) {
 
+	async.waterfall([
+		function(callback) {
+			fs.readFile('./images.json', 'ascii', callback);
+		},
+		function(json, callback) {
+			callback(null, eval(json))
+		},
+		function(images, callback) {
+			acceptable = false
 
-	var ratio = parseInt(req.params.width) / parseInt(req.params.height);
+			i = Math.floor(Math.random() * images.length);
+			// Loop through until we find an acceptable images
+			while(!acceptable) {
+				selectedImage = clone(images[i]);
+				if(selectedImage['height'] > req.params.height && selectedImage['width'] > req.params.width) {
+					acceptable = true
+				}
+				else {
+					i = Math.floor(Math.random() * images.length);
+					selectedImage = clone(images[i])
+				}
+			}
+			callback(null, selectedImage);
+		},
+		function(selectedImage, callback) {
+			var ratio = parseInt(req.params.width) / parseInt(req.params.height);
+			var selectedRatio = selectedImage['width'] / selectedImage['height'];
+
+			if (selectedRatio < 1 || ratio > 1) { 
+		 		var resizeParams = req.params.width
+			} else {
+					var resizeParams = 'x' + req.params.height
+			}
+
+			im.convert([selectedImage['filename'], '-resize', resizeParams, '-gravity', 'center', '-crop', req.params.width + 'x' + req.params.height + '+0+0', '+repage', './cache/tmp.jpg'], callback);
 
 
-	res.render('generate', {
-		title: 'Handsome!', 
-		width: req.params.width, 
-		height: req.params.height,
-		ratio: ratio
+		},
+		function(stdout, stderr, callback) {
+			var img = fs.readFileSync('./cache/tmp.jpg')
+			res.writeHead(200, {'Content-Type': 'image/jpeg'})
+			res.end(img, 'binary')
+		}
+	],
+	function(err, result) {
+		console.log('Errored: ' + err)
 	});
 
+}
+
+function clone(obj) {
+  var copy = {};
+  for (var attr in obj) {
+   if (obj.hasOwnProperty(attr)) {
+     copy[attr] = obj[attr];
+   }
+  }
+  return copy;
 }
